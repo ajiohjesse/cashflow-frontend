f8e78f6c494f0e8a725da53a8e50756828a26b79
Jesse Ajioh <ajiohjesse@gmail.com>
2025-01-25 11:38:39 +0100

    feat: add not found page

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,14 +1,19 @@
+import Footer from "@/components/footer";
+import PageHeader from "@/components/page-header";
 import { Button } from "@/components/ui/button";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { queryClient } from "@/lib/react-query";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import {
   ErrorRouteComponent,
+  Link,
   Outlet,
   createRootRoute,
   useRouter,
 } from "@tanstack/react-router";
+import { ArrowLeft } from "lucide-react";
 import { PostHogProvider } from "posthog-js/react";
 
 const postHogOptions = {
@@ -36,6 +41,7 @@ const ErrorBoundary: ErrorRouteComponent = ({ error, reset }) => {
 export const Route = createRootRoute({
   component: RootLayout,
   errorComponent: ErrorBoundary,
+  notFoundComponent: NotFoundComponent,
 });
 
 function RootLayout() {
@@ -49,8 +55,33 @@ function RootLayout() {
         <TooltipProvider>
           <Outlet />
         </TooltipProvider>
-        {/* <ReactQueryDevtools /> */}
+        <ReactQueryDevtools />
       </QueryClientProvider>
     </PostHogProvider>
+  );
+}
+
+function NotFoundComponent() {
+  return (
+    <>
+      <PageHeader />
+      <main className="grid h-svh place-items-center content-center gap-4 p-4">
+        <div className="flex flex-col items-center gap-4">
+          <h1 className="text-[clamp(4rem,20vw,9rem)] font-bold text-muted-foreground">
+            {"404 :("}
+          </h1>
+          <p className="text-center text-muted-foreground">
+            Oops! This page seems to be lost in space. Let's get you back home.
+          </p>
+          <Button size="lg" asChild>
+            <Link to="/">
+              <ArrowLeft />
+              Go Back
+            </Link>
+          </Button>
+        </div>
+      </main>
+      <Footer />
+    </>
   );
 }
